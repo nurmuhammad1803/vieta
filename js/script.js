f5a1e9a15583155d55bb811d493dcad9174004df
Nurmuhammad <72738918+nurmuhammad1803@users.noreply.github.com>
2021-09-17 20:36:47 +0500

    Added plugin fullpage js
    Header section,
    portfolio
    clients

---
+++ js/script.js
@@ -0,0 +1,15 @@
+jQuery(document).ready(function ($) {
+  $("#fullpage").fullpage({
+    sectionsColor: ['#fff', '#fff', '#fff']
+  });
+})
+1
+2
+3
+4
+5
+jQuery(document).ready(function ($) {
+  $("#fullpage").fullpage({
+    sectionsColor: ['#C63D0F', '#1BBC9B', '#7E8F7C']
+  });
+})
